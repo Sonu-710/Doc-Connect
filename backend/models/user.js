dfd238648c68832c2cdb2e5437c428377f609667
Mahendra singh <mahi07ms03@gmail.com>
2024-06-22 00:27:00 +0530

    data added for Doctors

--- backend/models/user.js
+++ backend/models/user.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
   phone: {
     type: String,
   },
-  experience: Number,
+  experience: String,
   photoUrl: {
     type: String,
     // default: "default.jpg",
