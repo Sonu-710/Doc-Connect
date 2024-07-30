b0d559328cec69863dc844a4f719b81dc770805c
samirhembrom <samirhembrom007@gmail.com>
2024-07-30 22:19:26 +0530

    added search bar feature

--- frontend/src/user/pages/Users.jsx
+++ frontend/src/user/pages/Users.jsx
@@ -5,10 +5,10 @@ import './Users.css';
 
 const Users = () => {
     return (
-        <>
+        <div className="container">
             <h2 className="headers">Medical <span className="d-block  color-p">Services</span></h2>
             <p className="description">Need instant medical help? Connect with local doctors near by. Just by confirming your nearest location you can get a list of all available doctors near by.</p>
-        </>)
+        </div>)
 }
 
 export default Users;
